50114efb7552cb29f59230358bf013994ed267c3
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-19 15:16:54 +1000

    Enable DNS support

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,9 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   azs             = ["us-west-2a", "us-west-2b"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 }
